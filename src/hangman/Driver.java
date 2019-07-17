7a2488014c182b9c5263ad35902aa007aa53e277
Munjal Shah <47045539+Munjal511@users.noreply.github.com>
2019-07-17 15:46:12 -0700

    Update Driver.java

--- src/hangman/Driver.java
+++ src/hangman/Driver.java
@@ -4,8 +4,10 @@ package hangman;
  * @author Munjal Shah
  */
 public class Driver {
+
     public static void main(String[] args) {
         String wordFile = "words.txt";
         Hangman hangman = new Hangman(wordFile);
+        hangman.startGame();
     }
 }
